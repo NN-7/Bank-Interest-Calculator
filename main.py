76aca427fe5ac3324f0aedc49999d8d538761db8
TheVulc4n <73201814+TheVulc4n@users.noreply.github.com>
2021-08-29 21:07:58 -0400

    Now writes to an Excel file.

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+# imports
+import xlsxwriter
+
 # variables
 p = 0
 r = None
 t = None
 
-# code
-def simple_interest(p,r,t,with_principal): # defining simple interest calc func
-    if(with_principal): # whether to return new balance or return interest
-        return (r*t/100+1)*p # returns total balance
-    return (r*t/100)*p # returns interest
+#objects
+interest_wb = xlsxwriter.Workbook(r'C:\Users\darks\git\Bank-Interest-Calculator\interest.xlsx') #making the workbook
 
+# code
+def worksheet_formatter(name):
+    worksheet = interest_wb.add_worksheet(name)
+    worksheet.write(0,0, 'Time')
+    worksheet.write(1,0, 'Principal')
+    worksheet.write(2,0, 'Rate')
+    worksheet.write(3,0, 'Interest')
+    worksheet.write(4,0, 'Balance')
+    worksheet.write(5,0, 'Increase')
+    return worksheet
+
+def simple_interest(p,r,t): # defining simple interest calc func
+    si_worksheet = worksheet_formatter('Simple Interest') # creating simple interest worksheet
+    si_worksheet.write(0,1, str(t))
+    si_worksheet.write(1,1, str(p))
+    si_worksheet.write(2,1, str(r))
+    si_worksheet.write(3,1, str((r*t/100)*p))
+    si_worksheet.write(4,1, str((r*t/100+1)*p))
+    si_worksheet.write(5,1, str((r*t/100)*p))
 
 p = int(input('Principal Amount? '))
 r = int(input('Rate? '))
 t = int(input('How long (years)? '))
-print('Interest is '+str(simple_interest(p,r,t,False)))
-print('Balance is '+str(simple_interest(p,r,t,True)))
+simple_interest(p,r,t)
+interest_wb.close()
